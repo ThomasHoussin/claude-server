91fac4f735c57079b04eaf96dbe695b41a81d6b2
Thomas Houssin <thomas.houssin@gmail.com>
2026-01-16 14:50:11 +0100

    Initial commit

---
+++ lib/claude-server-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ClaudeServerStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'ClaudeServerQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
